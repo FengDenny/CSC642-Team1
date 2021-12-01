fdeffbd8aa52a65dc741619e308e70269785a4fa
FengDenny <fengdennyy@gmail.com>
2021-11-30 20:59:40 -0800

    HashRouter for gh-pages deployment

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./mediaQueries.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import { HashRouter as Router } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
