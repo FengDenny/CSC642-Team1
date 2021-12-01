9127cb7badaec29b8cd71d452b16e9ed10daa4b0
FengDenny <fengdennyy@gmail.com>
2021-11-30 20:59:21 -0800

    react-router v6

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import "./App.css";
 import { Fragment } from "react";
 // need to use hashrouter for ghpages deployment
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  useRoutes,
+} from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./utility/navbar/Navbar";
+
 function App() {
-  return (
-    <div className='App'>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route exact path='/' element={<Navbar />} />
-          <Route exact path='/' element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+  let element = useRoutes([
+    {
+      path: "/",
+      element: (
+        <>
+          <Navbar />
+          <Home />
+        </>
+      ),
+    },
+  ]);
+  return <div className='App'>{element}</div>;
 }
 
 export default App;
