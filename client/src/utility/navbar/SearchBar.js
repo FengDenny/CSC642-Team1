b7d61c784f1c8ce6fda792b351e2ab2cb284796b
FengDenny <fengdennyy@gmail.com>
2021-11-30 17:13:24 -0800

    Search bar

---
+++ client/src/utility/navbar/SearchBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Navbar,
+  Container,
+  Row,
+  Col,
+  Nav,
+  Form,
+  Button,
+} from "react-bootstrap";
+
+export default function SearchBar() {
+  return (
+    <>
+      <Form>
+        <Form.Select
+          aria-label='Default select example'
+          className='search-select'
+        >
+          {/* will map this after we have data */}
+          <option>Open</option>
+          <option value='1'>One</option>
+          <option value='2'>Two</option>
+          <option value='3'>Three</option>
+        </Form.Select>
+        <Form.Control placeholder='Search' className='search-bar' type='text' />
+      </Form>
+    </>
+  );
+}
