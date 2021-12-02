0a50505cb44171979e8bf68bb14712488df56614
FengDenny <fengdennyy@gmail.com>
2021-12-02 14:45:48 -0800

    Hero section created

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { Container, Col, Row, Button, Image } from "react-bootstrap";
+import Explore from "../../image/explore.png";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='jumbotron gradient hero'>
+      {" "}
+      <Container>
+        <Row>
+          <Col>
+            <p className='lead white-color hero-text primary-font font-size-sm'>
+              Our goal is to help medical companies find participants for
+              trials, and for participants to get paid for finished trials.
+            </p>
+
+            <Button variant='outline-primary  bg-white-btn submit-btn'>
+              Submit Trials
+            </Button>
+            <Button variant='outline-primary outline-white apply-btn'>
+              Apply Now
+            </Button>
+          </Col>
+          <Col>
+            <Image src={Explore} rounded alt='explore' className='hero-img' />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
