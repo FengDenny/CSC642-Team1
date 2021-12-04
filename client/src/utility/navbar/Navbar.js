06d6a91c387496eb02e5d1cd31f42fff8c567d0f
FengDenny <fengdennyy@gmail.com>
2021-12-03 20:08:09 -0800

    Created modal switch from sign up/sign in

--- client/src/utility/navbar/Navbar.js
+++ client/src/utility/navbar/Navbar.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar, Container, Col, Nav, Button, Image } from "react-bootstrap";
 import Logo from "../../image/logo.png";
 import SearchBar from "./SearchBar";
-import ModalCentered from "../../components/modals/ModalForm";
+import ParticipantAuthModal from "../../components/modals/ParticipantAuthModal";
+import ClinicalAuthModal from "../../components/modals/ClinicalAuthModal";
+import SignInModal from "../../components/modals/SignInModal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -11,6 +13,9 @@ library.add(faGithub, faLinkedin, faSearch);
 
 export default function NavbarComponent() {
   const [showModal, setShowModal] = useState(false);
+  const [showClincicalModal, setShowClincicalModal] = useState(false);
+  const [showSignInModal, setShowSignInModal] = useState(false);
+  const [active, setActive] = useState("signup");
   return (
     <>
       <Navbar collapseOnSelect expand='lg' variant='dark' className='gradient'>
@@ -50,13 +55,23 @@ export default function NavbarComponent() {
               </Button>
             </Nav>
             <Nav>
-              <Button variant='outline-primary' className='signin-btn'>
+              <Button
+                variant='outline-primary'
+                className='signin-btn'
+                onClick={() => {
+                  setActive("signin");
+                  setShowSignInModal(true);
+                }}
+              >
                 SIGN IN
               </Button>
               <Button
                 variant='outline-primary'
                 className='signup-btn outline-white'
-                onClick={() => setShowModal(true)}
+                onClick={() => {
+                  setActive("clinical");
+                  setShowClincicalModal(true);
+                }}
               >
                 SIGN UP, IT’S FREE
               </Button>
@@ -64,19 +79,32 @@ export default function NavbarComponent() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <ModalCentered
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        title={"Welcome to Participaid"}
-        accountInfo={"participants"}
-        labelOne={"First name"}
-        labelTwo={"Last name"}
-        typeOne={"text"}
-        placeholderOne={"John"}
-        typeTwo={"text"}
-        placeholderTwo={"Doe"}
-        participants
-      ></ModalCentered>
+      {active === "signin" && (
+        <SignInModal
+          show={showSignInModal}
+          onHide={() => setShowSignInModal(false)}
+          setShowModal={setShowModal}
+          setActive={setActive}
+        />
+      )}{" "}
+      {active === "clinical" && (
+        <ClinicalAuthModal
+          show={showClincicalModal}
+          onHide={() => setShowClincicalModal(false)}
+          setShowModal={setShowModal}
+          setShowSignInModal={setShowSignInModal}
+          setActive={setActive}
+        />
+      )}
+      {active === "participant" && (
+        <ParticipantAuthModal
+          show={showModal}
+          onHide={() => setShowModal(false)}
+          setActive={setActive}
+          setShowClincicalModal={setShowClincicalModal}
+          setShowSignInModal={setShowSignInModal}
+        />
+      )}
     </>
   );
 }
