5d976dc8638d147c15cf472dd9050c6301b86fc9
FengDenny <fengdennyy@gmail.com>
2021-12-05 00:22:18 -0800

    Create authAction.js

---
+++ client/src/redux/actions/authAction.js
@@ -0,0 +1,20 @@
+export const setUserFirstName = (firstName) => ({
+  type: "SET_USER_FIRST_NAME",
+  firstName,
+});
+export const setUserLastName = (lastName) => ({
+  type: "SET_USER_LAST_NAME",
+  lastName,
+});
+export const setUserEmail = (email) => ({
+  type: "SET_USER_EMAIL",
+  email,
+});
+export const setUserPassword = (password) => ({
+  type: "SET_USER_PASSWORD",
+  password,
+});
+export const setUserID = (userId) => ({
+  type: "SET_USER_ID",
+  userId,
+});
