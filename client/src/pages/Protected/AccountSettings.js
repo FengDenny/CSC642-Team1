6a83c019f4f892cf5a53b2637d92be0b38433a91
FengDenny <fengdennyy@gmail.com>
2021-12-06 15:12:43 -0800

    Update AccountSettings.js

--- client/src/pages/Protected/AccountSettings.js
+++ client/src/pages/Protected/AccountSettings.js
@@ -4,12 +4,12 @@ import { useParams } from "react-router-dom";
 export default function AllTrials() {
   const { auth } = useSelector((state) => ({ ...state }));
   const { id } = useParams();
-  const ID = auth.userId === id;
-  console.log(ID);
+  // const ID = auth.userId === id;
+  // console.log(ID);
   return (
     <div>
       <h1>Hello, {auth.email}</h1>
-      <h2>ID: {auth.userId}</h2>
+      <h2>ID: {id}</h2>
     </div>
   );
 }
