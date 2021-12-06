5c841f3235a2ab135acc27fd56070e76d0b3a0b5
FengDenny <fengdennyy@gmail.com>
2021-12-06 00:45:59 -0800

    refactored sign in form to its own file

--- client/src/components/modals/SignInModal.js
+++ client/src/components/modals/SignInModal.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Modal, Button, Form, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
+import SignInFormData from "../form/SignInFormData";
 export default function signInModal(props) {
   const { onHide, setActive, setShowModal } = props;
   return (
@@ -17,42 +17,7 @@ export default function signInModal(props) {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group className='mb-3 form-group-control'>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type='email' placeholder='JohnDoe@health.gov' />
-            </Form.Group>
-            <Form.Group className='mb-3 form-group-control'>
-              <Form.Label>Password</Form.Label>
-              <Button
-                onClick={() => {
-                  //   setActive("participant");
-                  //   setShowModal(true);
-                }}
-                className='button-link left120'
-              >
-                Forgot Password?
-              </Button>
-              <Form.Control type='password' placeholder='*******' />
-            </Form.Group>
-            <Form.Group className='mb-3 form-group-control'>
-              <Button variant='outline-primary' className='primary-color-btn'>
-                SIGN IN
-              </Button>
-            </Form.Group>
-            <Form.Group className='mb-3 form-group-control form-group-control-last top5'>
-              <Form.Label>Need an account?</Form.Label>
-              <Button
-                onClick={() => {
-                  setActive("participant");
-                  setShowModal(true);
-                }}
-                className='button-link '
-              >
-                Sign Up Free
-              </Button>
-            </Form.Group>
-          </Form>
+          <SignInFormData setActive={setActive} setShowModal={setShowModal} />
         </Modal.Body>
         <Modal>
           <Button onClick={onHide}>Close</Button>
