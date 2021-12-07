ab303a3a61bf1e2b7cbcff5c8c82e0080db0388f
FengDenny <fengdennyy@gmail.com>
2021-12-06 17:19:34 -0800

    Create AboutLatestTrials.js

---
+++ client/src/pages/Trials/AboutLatestTrials.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import trials from "../../data/latest-trials.json";
+
+export default function AboutLatestTrials() {
+  const { id } = useParams();
+  const latest = trials["Latest Trials"].find(
+    (trials) => trials.id === Number(id)
+  );
+  return (
+    <div>
+      {latest ? (
+        <div>
+          <h1>{latest.title}</h1>
+          <p>{latest.description}</p>
+        </div>
+      ) : (
+        <h1> Sorry, but this trials doesn't exist yet!</h1>
+      )}
+    </div>
+  );
+}
