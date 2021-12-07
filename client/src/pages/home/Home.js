248b39a144e44b98039cb7c47c04e890b9d040e8
FengDenny <fengdennyy@gmail.com>
2021-12-07 11:49:56 -0800

    Restrict certain features based on account status

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -5,69 +5,112 @@ import LatestCard from "../Card/LatestCard";
 import ConflictCard from "../Card/ConflictCard";
 import FutureCard from "../Card/FutureCards";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import CreateAccountModal from "../../components/modals/CreateAccountModal";
 
 export default function Home() {
   // submit
-  const [showSubmitModal, setShowSubmitModal] = useState(false);
-  // const [showApplyModal, setShowSubmitModal] = useState(false);
-  const [active, setActive] = useState("submit-trials");
+  const [alertModal, setAlertModal] = useState(false);
+  const [alertApplyModal, setAlertApplyModal] = useState(false);
+  const [active, setActive] = useState("participants");
+  const { auth } = useSelector((state) => ({ ...state }));
   const navigate = useNavigate();
+
   return (
-    <main>
-      <section className='jumbotron gradient hero'>
-        <Container fluid>
-          <Col>
-            <p className='lead white-color hero-text primary-font font-size-sm'>
-              Our goal is to help medical companies find participants for
-              trials, and for participants to get paid for finished trials.
-            </p>
-            <div className='button-mobile'>
-              <Button
-                variant='outline-primary  bg-white-btn submit-btn'
-                onClick={() => navigate("/submit-trials")}
-              >
-                Submit Trials
-              </Button>
-              <Button variant='outline-primary outline-white apply-btn'>
-                Apply Now
-              </Button>
+    <>
+      <main>
+        <section className='jumbotron gradient hero'>
+          <Container fluid>
+            <Col>
+              <p className='lead white-color hero-text primary-font font-size-sm'>
+                Our goal is to help medical companies find participants for
+                trials, and for participants to get paid for finished trials.
+              </p>
+              <div className='button-mobile'>
+                <Button
+                  variant='outline-primary  bg-white-btn submit-btn'
+                  onClick={() => {
+                    auth.clinicalLoggedIn
+                      ? navigate("/submit-trials")
+                      : auth.isLoggedIn
+                      ? setActive("participants") || setAlertModal(true)
+                      : navigate("/submit-trials");
+                  }}
+                >
+                  Submit Trials
+                </Button>
+                <Button
+                  variant='outline-primary outline-white apply-btn'
+                  onClick={() => {
+                    auth.isLoggedIn
+                      ? navigate("/participate")
+                      : auth.clinicalLoggedIn
+                      ? setActive("clinical") || setAlertApplyModal(true)
+                      : navigate("/participate");
+                  }}
+                >
+                  Apply Now
+                </Button>
+              </div>
+            </Col>
+            <Col>
+              <Image src={Explore} rounded alt='explore' className='hero-img' />
+            </Col>
+          </Container>
+        </section>
+        <section>
+          <Container fluid>
+            <h2 className='margin-left-50 header-color primary-font font-weight-bold'>
+              Latest Trials
+            </h2>
+            <div className='d-flex flex-wrap'>
+              <LatestCard />
             </div>
-          </Col>
-          <Col>
-            <Image src={Explore} rounded alt='explore' className='hero-img' />
-          </Col>
-        </Container>
-      </section>
-      <section>
-        <Container fluid>
-          <h2 className='margin-left-50 header-color primary-font font-weight-bold'>
-            Latest Trials
-          </h2>
-          <div className='d-flex flex-wrap'>
-            <LatestCard />
-          </div>{" "}
-        </Container>
-      </section>
-      <section className='margin-top-10'>
-        <Container fluid>
-          <h2 className='margin-left-50 header-color primary-font font-weight-bold'>
-            Participate without conflictions
-          </h2>
-          <div className='d-flex flex-wrap'>
-            <ConflictCard />
-          </div>{" "}
-        </Container>
-      </section>
-      <section className='margin-top-10'>
-        <Container fluid>
-          <h2 className='margin-left-50 header-color primary-font font-weight-bold'>
-            Discover future trials
-          </h2>
-          <div className='d-flex flex-wrap'>
-            <FutureCard />
-          </div>
-        </Container>
-      </section>
-    </main>
+          </Container>
+        </section>
+        <section className='margin-top-10'>
+          <Container fluid>
+            <h2 className='margin-left-50 header-color primary-font font-weight-bold'>
+              Participate without conflictions
+            </h2>
+            <div className='d-flex flex-wrap'>
+              <ConflictCard />
+            </div>
+          </Container>
+        </section>
+        <section className='margin-top-10'>
+          <Container fluid>
+            <h2 className='margin-left-50 header-color primary-font font-weight-bold'>
+              Discover future trials
+            </h2>
+            <div className='d-flex flex-wrap'>
+              <FutureCard />
+            </div>
+          </Container>
+        </section>
+      </main>
+
+      {active === "participants" && (
+        <CreateAccountModal
+          fname={auth.firstName}
+          show={alertModal}
+          onHide={() => {
+            setAlertModal(false);
+          }}
+          setActive={setActive}
+        />
+      )}
+
+      {active === "clinical" && (
+        <CreateAccountModal
+          cname={auth.companyName}
+          show={alertApplyModal}
+          onHide={() => {
+            setAlertApplyModal(false);
+          }}
+          setActive={setActive}
+        />
+      )}
+    </>
   );
 }
