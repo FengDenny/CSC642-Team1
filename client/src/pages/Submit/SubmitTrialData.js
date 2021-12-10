08b5919de34b82b15596274e15ad53aee4df09b8
FengDenny <fengdennyy@gmail.com>
2021-12-09 20:05:34 -0800

    Linked all trial submit actions to submit form

--- client/src/pages/Submit/SubmitTrialData.js
+++ client/src/pages/Submit/SubmitTrialData.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import SubmitTrial from "./SubmitTrial";
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { nanoid } from "nanoid/async";
 import {
   setTrialTitle,
@@ -14,6 +15,7 @@ import {
   setTrialStatus,
   setTrialID,
   setTrialDate,
+  setTrialPoster,
 } from "../../redux/actions/trailSubmitAction";
 
 // import { addCheck } from "./getArray";
@@ -22,13 +24,28 @@ export default function SubmitTrialData() {
   const [title, setTitle] = useState("");
   const [participants, setParticipants] = useState("");
   const [description, setDescription] = useState("");
-  const [requirement, setRequirement] = useState("");
-  const [notQualified, setNotQualified] = useState([]);
+  const [qualified, setQualified] = useState("");
+  const [notQualified, setNotQualified] = useState("");
   const [payout, setPayout] = useState("");
+  const [prerequisite, setPrerequisite] = useState("");
+  const [location, setLocation] = useState("");
+  const [status, setStatus] = useState("");
+  // date
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  // check is id is the same as poster id
+  const { auth } = useSelector((state) => ({ ...state }));
+  const { clinicalID } = auth;
+
   const handleFormSubmit = async (e) => {
-    const splitReqirements = requirement.split(",");
+    const splitReqirements = qualified.split(",");
+    const splitNonReqirements = notQualified.split(",");
+    const splitPrerequisite = prerequisite.split(",");
     const Id = await nanoid(5);
+
     e.preventDefault();
     dispatch(setTrialID(Id));
     dispatch(setTrialTitle(title));
@@ -36,7 +53,13 @@ export default function SubmitTrialData() {
     dispatch(setTrialPayout(payout));
     dispatch(setTrialDescription(description));
     dispatch(setTrialQualified(splitReqirements));
-    dispatch(setTrialNotQualified(notQualified));
+    dispatch(setTrialNotQualified(splitNonReqirements));
+    dispatch(setTrialPrerequisite(splitPrerequisite));
+    dispatch(setTrialLocation(location));
+    dispatch(setTrialStatus(status));
+    dispatch(setTrialDate(start, end));
+    dispatch(setTrialPoster(clinicalID));
+    navigate("/");
   };
 
   return (
@@ -50,10 +73,20 @@ export default function SubmitTrialData() {
       setPayout={setPayout}
       description={description}
       setDescription={setDescription}
-      setRequirement={setRequirement}
-      requirement={requirement}
+      setQualified={setQualified}
+      qualified={qualified}
       notQualified={notQualified}
       setNotQualified={setNotQualified}
+      prerequisite={prerequisite}
+      setPrerequisite={setPrerequisite}
+      location={location}
+      setLocation={setLocation}
+      status={status}
+      setStatus={setStatus}
+      start={start}
+      setStart={setStart}
+      end={end}
+      setEnd={setEnd}
     />
   );
 }
