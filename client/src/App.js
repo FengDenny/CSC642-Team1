09edcb3d29f52d67cd21db58b90b1145e4ef073f
FengDenny <fengdennyy@gmail.com>
2021-12-09 19:59:53 -0800

    show approved trials to clinical users

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import {
 import Home from "./pages/home/Home";
 import SubmitTrialData from "./pages/Submit/SubmitTrialData";
 import AccountSettings from "./pages/Protected/AccountSettings";
+import TrialSubmitted from "./pages/Protected/clinical/TrialSubmitted";
 import AboutLatestTrials from "./pages/Trials/AboutLatestTrials";
 import AboutConflictTrials from "./pages/Trials/AboutConflictTrials";
 import AboutDiscoverTrials from "./pages/Trials/AboutDiscoverTrials";
@@ -19,7 +20,7 @@ import Footer from "./utility/footer/Footer";
 import { useSelector } from "react-redux";
 
 function App() {
-  const { auth } = useSelector((state) => ({ ...state }));
+  const { auth, submit } = useSelector((state) => ({ ...state }));
   let element = useRoutes([
     {
       path: "/",
@@ -42,6 +43,19 @@ function App() {
       element:
         auth.isLoggedIn || auth.clinicalLoggedIn ? (
           <AccountSettings />
+        ) : (
+          <Navigate to='/' />
+        ),
+    },
+    {
+      path: "/approved-trials/:userID/",
+      element: auth.clinicalLoggedIn ? <TrialSubmitted /> : <Navigate to='/' />,
+    },
+    {
+      path: "/approved-trials/:userID/latest/:id",
+      element:
+        submit.poster === auth.clinicalID ? (
+          <AboutLatestTrials />
         ) : (
           <Navigate to='/' />
         ),
