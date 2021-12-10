bbb5521a9df12daea1ed120b9e88de68d64e312c
MarcoMarinoSFSU <70168839+MarcoMarinoSFSU@users.noreply.github.com>
2021-12-10 12:06:34 -0800

    updated auth actions

--- client/src/redux/actions/authAction.js
+++ client/src/redux/actions/authAction.js
@@ -10,6 +10,22 @@ export const setUserEmail = (email) => ({
   type: "SET_USER_EMAIL",
   email,
 });
+export const setUserAddress = (address) => ({
+  type: "SET_USER_ADDRESS",
+  address,
+});
+export const setUserAptNo = (aptno) => ({
+  type: "SET_USER_APTNO",
+  aptno,
+});
+export const setUserCityState = (citystate) => ({
+  type: "SET_USER_CITYSTATE",
+  citystate,
+});
+export const setUserZipcode = (zipcode) => ({
+  type: "SET_USER_ZIPCODE",
+  zipcode,
+});
 export const setUserPassword = (password) => ({
   type: "SET_USER_PASSWORD",
   password,
@@ -18,7 +34,6 @@ export const setUserID = (userId) => ({
   type: "SET_USER_ID",
   userId,
 });
-
 export const setIsLoggedIn = (isLoggedIn) => ({
   type: "SET_USER_IS_LOGGED_IN",
   isLoggedIn,
@@ -45,3 +60,7 @@ export const setClinicalLoggedIn = (clinicalLoggedIn) => ({
   type: "SET_USER_CLINICAL_LOGGED_IN",
   clinicalLoggedIn,
 });
+
+export const deleteUserInfo = () =>({
+  type: 'DELETE_USER_INFO',
+})
