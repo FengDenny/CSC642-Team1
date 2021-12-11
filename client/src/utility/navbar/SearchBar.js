0f3cc18bf4eeb60b1364cc4c0e136c5a62555e05
FengDenny <fengdennyy@gmail.com>
2021-12-10 17:53:25 -0800

    select search is working

--- client/src/utility/navbar/SearchBar.js
+++ client/src/utility/navbar/SearchBar.js
@@ -1,36 +1,49 @@
-import React from "react";
-import {
-  Navbar,
-  Container,
-  Row,
-  Col,
-  Nav,
-  Form,
-  Button,
-} from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+// Redux
+import { useDispatch } from "react-redux";
+import { setSearchSelect } from "../../redux/actions/searchAction";
+
+//react-router-dom
+import { useNavigate } from "react-router-dom";
+
+// FontAwesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 library.add(faGithub, faLinkedin, faSearch);
 
-export default function SearchBar() {
+export default function SearchBar({ value, setValue }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmitSearch = (e) => {
+    e.preventDefault();
+    dispatch(setSearchSelect(value));
+    // console.log(select[0].value);
+    navigate("/search");
+  };
+
   return (
     <>
       <Form className='search-form'>
         <Form.Select
           aria-label='Default select example'
           className='search-select'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         >
-          {/* will map this after we have data */}
-          <option>Open</option>
-          <option value='1'>One</option>
-          <option value='2'>Two</option>
-          <option value='3'>Three</option>
+          <option value='Recruiting'>Recruiting</option>
+          <option value='Not Recruiting'>Not Recruiting</option>
         </Form.Select>
         <Form.Control placeholder='Search' className='search-bar' type='text' />
         <Button variant='primary' className='search-button'>
-          <FontAwesomeIcon icon={faSearch} size='1x' />
+          <FontAwesomeIcon
+            icon={faSearch}
+            size='1x'
+            onClick={(e) => handleSubmitSearch(e)}
+          />
         </Button>
       </Form>
     </>
