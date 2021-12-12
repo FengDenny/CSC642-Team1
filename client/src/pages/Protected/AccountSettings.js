472e26ac418a20e7a8d9a0b95b9f8714891f653a
FengDenny <fengdennyy@gmail.com>
2021-12-11 16:00:13 -0800

    updated but states not firing

--- client/src/pages/Protected/AccountSettings.js
+++ client/src/pages/Protected/AccountSettings.js
@@ -9,25 +9,25 @@ export default function AccountSettings({
   handlePersonalDataSubmit,
   handleUpdateAccountSubmit,
   deleteUserInfo,
-            firstName,
-            setFirstName,
-            lastName,
-            setLastName,
-            email,
-            setEmail,
-            address,
-            setAddress,
-            aptno,
-            setAptno,
-            citystate,
-            setCityState,
-            zipcode,
-            setZipcode,
-            password,
-            setPassword,
+  firstName,
+  setFirstName,
+  lastName,
+  setLastName,
+  email,
+  setEmail,
+  address,
+  setAddress,
+  aptno,
+  setAptno,
+  citystate,
+  setCityState,
+  zipcode,
+  setZipcode,
+  password,
+  setPassword,
+  updateEmail,
+  setUpdateEmail,
 }) {
-  const { auth } = useSelector((state) => ({ ...state }));
-  const { id } = useParams();
   const [validated, SetValidated] = useState(false);
 
   const handleSubmit = (event) => {
@@ -41,138 +41,159 @@ export default function AccountSettings({
   };
 
   const comparePasswords = () => {
-    if(document.getElementById('curPassword').value == password){
+    if (document.getElementById("curPassword").value === password) {
       return true;
-    }else{
+    } else {
       return false;
     }
-  }
+  };
 
-  const submitPasswordUpdate = (event) =>{
-    if(comparePasswords){
-      setPassword(document.getElementById('newPassword').value);
+  const SubmitPasswordUpdate = (event) => {
+    if (comparePasswords) {
+      setPassword(document.getElementById("newPassword").value);
       handleUpdateAccountSubmit(event);
     }
+  };
 
-  }
-
-  function deleteAlert(){
-
+  function deleteAlert() {
     var conf = window.confirm("Are you sure you want to delete your profile?");
-    if(conf == true){
-      deleteUserInfo()
+    if (conf === true) {
+      deleteUserInfo();
     }
   }
 
-
-  const personalProfile = (
+  const PersonalProfile = () => (
     <>
-    <Container >
+      <Container>
         <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Row >
+          <Row>
             <Form.Group as={Col} xs={3} controlId='userFirstName'>
               <Form.Label>First Name</Form.Label>
-              <Form.Control 
-              type='text' 
-              value={firstName} 
-              onChange={(e)=>{setFirstName(e.target.value)}} 
-              required />
+              <Form.Control
+                type='text'
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
             </Form.Group>
 
             <Form.Group as={Col} xs={3} controlId='userLastname'>
               <Form.Label>Last Name</Form.Label>
-              <Form.Control 
-              type='text' 
-              value={lastName}
-              onChange={(e)=>{setLastName(e.target.value)}}
-              required />
+              <Form.Control
+                type='text'
+                value={lastName}
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                }}
+                required
+              />
             </Form.Group>
 
             <Form.Group as={Col} xs={6} controlId='userEmail'>
               <Form.Label>Email</Form.Label>
-              <Form.Control 
-              type='email' 
-              value={email}
-              onChange={(e)=>{setEmail(e.target.value)}} 
-              required />
+              <Form.Control
+                type='email'
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                required
+              />
             </Form.Group>
           </Row>
-          <Row >
+          <Row>
             <Form.Group as={Col} controlId='userAddress'>
               <Form.Label>Street Address</Form.Label>
-              <Form.Control 
-              type='text' 
-              value={address}
-              onChange={(e)=>{setAddress(e.target.value)}} 
-              required />
+              <Form.Control
+                type='text'
+                value={address}
+                onChange={(e) => {
+                  setAddress(e.target.value);
+                }}
+                required
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId='userAptno'>
               <Form.Label>Apt, Suite, Bldg</Form.Label>
-              <Form.Control 
-              type='text' 
-              value={aptno}
-              onChange={(e)=>{setAptno(e.target.value)}} 
-              required />
+              <Form.Control
+                type='text'
+                value={aptno}
+                onChange={(e) => {
+                  setAptno(e.target.value);
+                }}
+                required
+              />
             </Form.Group>
           </Row>
-          <Row >
+          <Row>
             <Form.Group as={Col} controlId='userCityState'>
               <Form.Label>City, State</Form.Label>
-              <Form.Control 
-              type='text' 
-              value={citystate}
-              onChange={(e)=>{setCityState(e.target.value)}}
-              required />
+              <Form.Control
+                type='text'
+                value={citystate}
+                onChange={(e) => {
+                  setCityState(e.target.value);
+                }}
+                required
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId='userZipcode'>
               <Form.Label>zipcode</Form.Label>
-              <Form.Control 
-              type='text' 
-              value={zipcode} 
-              onChange={(e)=>{setZipcode(e.target.value)}}
-              required />
+              <Form.Control
+                type='text'
+                value={zipcode}
+                onChange={(e) => {
+                  setZipcode(e.target.value);
+                }}
+                required
+              />
             </Form.Group>
-          </Row><br/>
+          </Row>
+          <br />
           <Row>
-            <Col >
-              <Button 
-              className="primary-color-btn"
-              disabled={
-                !firstName ||
-                !lastName ||
-                !email ||
-                !address ||
-                !aptno ||
-                !citystate ||
-                !zipcode
-              }
-              onClick={(e)=>{handlePersonalDataSubmit(e);}}
-              >Save Changes</Button>
+            <Col>
+              <Button
+                className='primary-color-btn'
+                // disabled={
+                //   !firstName ||
+                //   !lastName ||
+                //   !address ||
+                //   !aptno ||
+                //   !citystate ||
+                //   !zipcode
+                // }
+                onClick={(e) => {
+                  handlePersonalDataSubmit(e);
+                }}
+              >
+                Save Changes
+              </Button>
             </Col>
-            
           </Row>
         </Form>
       </Container>
     </>
   );
 
-  const updateProfile = (
+  const UpdateProfile = () => (
     <>
-    <Container >
-        <Form >
+      <Container>
+        <Form>
           <Row>
             <h3>Update Your Email</h3>
           </Row>
           <Row>
-            <Form.Group  controlId='updateEmail'>
+            <Form.Group controlId='updateEmail'>
               <Form.Label>Email</Form.Label>
               <Form.Control
-               type='email' 
-               value={email}
-               onChange={(e)=>{setEmail(e.target.value)}}
-               ></Form.Control>
+                type='email'
+                value={updateEmail}
+                onChange={(e) => {
+                  setUpdateEmail(e.target.value);
+                }}
+              ></Form.Control>
             </Form.Group>
           </Row>
           <hr />
@@ -180,116 +201,131 @@ export default function AccountSettings({
             <h3>Update Your Password</h3>
           </Row>
           <Row>
-            <Form.Group  id='curPassword'>
+            <Form.Group id='curPassword'>
               <Form.Label>Current Password</Form.Label>
               <Form.Control
-               type='password'
-              placeholder="Current Password"></Form.Control>
+                type='password'
+                placeholder='Current Password'
+              ></Form.Control>
             </Form.Group>
           </Row>
           <Row>
             <Form.Group id='newPassword'>
               <Form.Label>New Password</Form.Label>
               <Form.Control
-              type='password'
-              placeholder="New Password"></Form.Control>
+                type='password'
+                placeholder='New Password'
+              ></Form.Control>
             </Form.Group>
           </Row>
-          
-            <hr />
-            <Row >
-              <Col><h3>Delete Your Account</h3></Col>
-            </Row>
-            <Row>
-              <Button 
-                className="primary-color-btn"
-                id="DeleteButton"
-                onClick={()=>{deleteAlert()}}
-                >Delete Account</Button>
-              
-              
-            </Row><hr style={{width:"340px"}} ></hr>
-            <Row>
-              <Button
-              className="primary-color-btn"
+
+          <hr />
+          <Row>
+            <Col>
+              <h3>Delete Your Account</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Button
+              className='primary-color-btn'
+              id='DeleteButton'
+              onClick={() => {
+                deleteAlert();
+              }}
+            >
+              Delete Account
+            </Button>
+          </Row>
+          <hr style={{ width: "340px" }}></hr>
+          <Row>
+            <Button
+              className='primary-color-btn'
               disabled={
-                !email ||
-                !document.getElementById('curPassword').value ||
-                !document.getElementById('newPassword').value
+                !updateEmail ||
+                !document.getElementById("curPassword").value ||
+                !document.getElementById("newPassword").value
               }
-              onClick={(e)=>{submitPasswordUpdate(e)}}
-              >Update Account</Button>
-            </Row>
+              onClick={(e) => {
+                SubmitPasswordUpdate(e);
+              }}
+            >
+              Update Account
+            </Button>
+          </Row>
         </Form>
       </Container>
     </>
   );
 
-  const showPersonalProfile = () => {
-    document.getElementById("Details Button").style.textDecoration="underline";
-    document.getElementById("Details Button").style.textDecorationColor='#0b97ab';
-    document.getElementById("Update Account Button").style.textDecoration="";
-    ReactDOM.render(personalProfile, document.getElementById("infotab"));
+  const ShowPersonalProfile = () => {
+    document.getElementById("Details Button").style.textDecoration =
+      "underline";
+    document.getElementById("Details Button").style.textDecorationColor =
+      "#0b97ab";
+    document.getElementById("Update Account Button").style.textDecoration = "";
+    ReactDOM.render(PersonalProfile(), document.getElementById("infotab"));
   };
 
-  const showUpdateProfile = () => {
-    document.getElementById("Update Account Button").style.textDecoration="underline";
-    document.getElementById("Update Account Button").style.textDecorationColor='#0b97ab';
-    document.getElementById("Details Button").style.textDecoration="";
-    ReactDOM.render(updateProfile, document.getElementById("infotab"));
+  const ShowUpdateProfile = () => {
+    document.getElementById("Update Account Button").style.textDecoration =
+      "underline";
+    document.getElementById("Update Account Button").style.textDecorationColor =
+      "#0b97ab";
+    document.getElementById("Details Button").style.textDecoration = "";
+    ReactDOM.render(UpdateProfile(), document.getElementById("infotab"));
   };
-
-  // const ID = auth.userId === id;
-  // console.log(ID);
-
   return (
     <>
-    <br/>
+      <br />
 
-    <Container >
-          <Row >
-            <Col xs={9}></Col>
-            <Col xs={{offset:3}}></Col>
-            <Col xs={{offset:1}}></Col>
-            <Col xs={10} >
-                    
-              <h1>Account Settings</h1>
-              </Col>
-            
-          </Row>
-    </Container><br/><br/><br/><br/><br/>
+      <Container>
+        <Row>
+          <Col xs={9}></Col>
+          <Col xs={{ offset: 3 }}></Col>
+          <Col xs={{ offset: 1 }}></Col>
+          <Col xs={10}>
+            <h1>Account Settings</h1>
+          </Col>
+        </Row>
+      </Container>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
-    <Container >
-    
-      <Col lg={{offset:2}}>
-      
-      <div 
-        class='p-3 nav  '
-      >
-        <ul class='nav nav-pills flex-column'>
-          
-          <li>
-            <button class='nav-link link-dark' onClick={showPersonalProfile}>
-              
-              <h3 className="header-color" id="Details Button">Personal Details</h3>
-            </button>
-          </li>
-          <li>
-            <button
-              href='#'
-              class='nav-link link-dark'
-              onClick={showUpdateProfile}
-            >
-              <h3 className="header-color" id="Update Account Button">Update Account</h3>
-            </button>
-          </li>
-        </ul>
+      <Container>
+        <Col lg={{ offset: 2 }}>
+          <div class='p-3 nav  '>
+            <ul class='nav nav-pills flex-column'>
+              <li>
+                <button
+                  class='nav-link link-dark'
+                  onClick={ShowPersonalProfile}
+                >
+                  <h3 className='header-color' id='Details Button'>
+                    Personal Details
+                  </h3>
+                </button>
+              </li>
+              <li>
+                <button
+                  href='#'
+                  class='nav-link link-dark'
+                  onClick={ShowUpdateProfile}
+                >
+                  <h3 className='header-color' id='Update Account Button'>
+                    Update Account
+                  </h3>
+                </button>
+              </li>
+            </ul>
 
-        <div class='d-flex flex-column flex-shrink-0 p-3 ' id='infotab'> </div>
-
-      </div>
-      </Col>
-      
+            <div class='d-flex flex-column flex-shrink-0 p-3 ' id='infotab'>
+              {" "}
+            </div>
+          </div>
+        </Col>
       </Container>
     </>
   );
--- client/src/pages/Protected/AccountSettingsData.js
+++ client/src/pages/Protected/AccountSettingsData.js
@@ -1,72 +1,75 @@
 import React, { useState, useEffect } from "react";
-import SubmitTrial from "./SubmitTrial";
 import { useSelector, useDispatch } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
-import{
-    setUserFirstName,
-    setUserLastName,
-    setUserEmail,
-    setUserAddress,
-    setUserAptNo,
-    setUserCityState,
-    setUserZipcode,
-    setUserPassword,
-    deleteUserInfo
-  } from "../../redux/actions/authAction"
+import {
+  setUserFirstName,
+  setUserLastName,
+  setUserEmail,
+  setUserAddress,
+  setUserAptNo,
+  setUserCityState,
+  setUserZipcode,
+  setUserPassword,
+  deleteUserInfo,
+} from "../../redux/actions/authAction";
 import AccountSettings from "./AccountSettings";
 
-  export default function submitAccountData(){
-      const[firstName, setFirstName]= useState("");
-      const[lastName, setLastName]= useState("");
-      const[email, setEmail]= useState("");
-      const[address, setAddress]= useState("");
-      const[citystate, setCityState]= useState("");
-      const[aptno, setAptno]= useState("");
-      const[zipcode, setZipcode]= useState("");
-      const[password, setPassword]= useState("");
-  
-      const handlePersonalDataSubmit = async (e) =>{
-          e.preventDefault();
-          dispatch(setUserFirstName(firstName));
-          dispatch(setUserLastName(lastName));
-          dispatch(setUserEmail(email));
-          dispatch(setUserAddress(address));
-          dispatch(setUserAptNo(aptno));
-          dispatch(setUserCityState(citystate));
-          dispatch(setUserZipcode(zipcode));
-          dispatch(setUserPassword(password));
-          Navigate("/");
-      };
+export default function AccountSettingsData() {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [updateEmail, setUpdateEmail] = useState();
+  const [address, setAddress] = useState();
+  const [citystate, setCityState] = useState();
+  const [aptno, setAptno] = useState();
+  const [zipcode, setZipcode] = useState();
+  const [password, setPassword] = useState();
 
-      const handleUpdateAccountSubmit = async (e) =>{
-          e.preventDefault();
-          dispatch(setUserEmail(email));
-          dispatch(setUserPassword(password));
-          Navigate("/");
-      }
+  const dispatch = useDispatch();
 
-      return(
-          <AccountSettings
-            handlePersonalDataSubmit={handlePersonalDataSubmit}
-            handleUpdateAccountSubmit={handleUpdateAccountSubmit}
-            deleteUserInfo={deleteUserInfo}
-            firstName={firstName}
-            setFirstName={setFirstName}
-            lastName={lastName}
-            setLastName={setLastName}
-            email={email}
-            setEmail={setEmail}
-            address={address}
-            setAddress={setAddress}
-            aptno={aptno}
-            setAptno={setAptno}
-            citystate={citystate}
-            setCityState={setCityState}
-            zipcode={zipcode}
-            setZipcode={setZipcode}
-            password={password}
-            setPassword={setPassword}
-          />
-      );
-  
-    }
+  const handlePersonalDataSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(setUserFirstName(firstName));
+    dispatch(setUserLastName(lastName));
+    dispatch(setUserEmail(email));
+    dispatch(setUserAddress(address));
+    dispatch(setUserAptNo(aptno));
+    dispatch(setUserCityState(citystate));
+    dispatch(setUserZipcode(zipcode));
+    dispatch(setUserPassword(password));
+    Navigate("/");
+  };
+
+  const handleUpdateAccountSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(setUserEmail(email));
+    dispatch(setUserPassword(password));
+    Navigate("/");
+  };
+
+  return (
+    <AccountSettings
+      handlePersonalDataSubmit={handlePersonalDataSubmit}
+      handleUpdateAccountSubmit={handleUpdateAccountSubmit}
+      deleteUserInfo={deleteUserInfo}
+      firstName={firstName}
+      setFirstName={setFirstName}
+      lastName={lastName}
+      setLastName={setLastName}
+      email={email}
+      setEmail={setEmail}
+      updateEmail={updateEmail}
+      setUpdateEmail={setUpdateEmail}
+      address={address}
+      setAddress={setAddress}
+      aptno={aptno}
+      setAptno={setAptno}
+      citystate={citystate}
+      setCityState={setCityState}
+      zipcode={zipcode}
+      setZipcode={setZipcode}
+      password={password}
+      setPassword={setPassword}
+    />
+  );
+}
