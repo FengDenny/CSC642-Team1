f90e924dfdfd4f1748ba866541dc837a9bab3c50
FengDenny <fengdennyy@gmail.com>
2021-12-11 19:29:38 -0800

    Update SearchTrials.js

--- client/src/components/search/SearchTrials.js
+++ client/src/components/search/SearchTrials.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { Container, Col, Row, Button, Image } from "react-bootstrap";
-export default function SearchTrials({ latest, conflict, future }) {
+import { Container } from "react-bootstrap";
+export default function SearchTrials({
+  latest,
+  conflict,
+  future,
+  text,
+  value,
+}) {
   return (
     <Container>
       {latest || conflict || future ? (
         <div className='d-flex flex-wrap'>
           {latest} {conflict} {future}
         </div>
-      ) : null}
+      ) : (
+        <div className='d-flex flex-wrap'>Sorry, cannot be found</div>
+      )}
     </Container>
   );
 }
