660860278406a2f1bbeebd05ebcba32f95d447be
FengDenny <fengdennyy@gmail.com>
2021-12-11 17:31:52 -0800

    Create AppledTrialFormData.js

---
+++ client/src/components/form/AppledTrialFormData.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import AppliedTrialForm from "./AppliedTrialForm";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  setAppliedName,
+  setAppliedEmail,
+  setAppliedID,
+  setAppliedURL,
+} from "../../redux/actions/appliedAction";
+import { useParams } from "react-router-dom";
+
+export default function AppledTrialFormData() {
+  const dispatch = useDispatch();
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const [emailApply, setEmailApply] = useState("");
+  const { auth } = useSelector((state) => ({ ...state }));
+
+  const { firstName, lastName, email } = auth;
+  const { id } = useParams();
+
+  const handleApply = (e) => {
+    e.preventDefault();
+    dispatch(setAppliedEmail(emailApply));
+    dispatch(setAppliedName(first, last));
+    dispatch(setAppliedID(id));
+    dispatch(
+      setAppliedURL(
+        window.location.pathname.slice(
+          0,
+          window.location.pathname.lastIndexOf("/")
+        )
+      )
+    );
+  };
+
+  return (
+    <>
+      <AppliedTrialForm
+        first={first}
+        last={last}
+        emailApply={emailApply}
+        setFirst={setFirst}
+        setLast={setLast}
+        setEmailApply={setEmailApply}
+        handleApply={handleApply}
+        firstName={firstName}
+        lastName={lastName}
+        email={email}
+      />
+    </>
+  );
+}
