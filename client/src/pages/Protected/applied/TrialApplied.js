4b2c33d68850699ad838d2d8f65e8c3a8765153c
FengDenny <fengdennyy@gmail.com>
2021-12-12 16:30:40 -0800

    created applied none card

--- client/src/pages/Protected/applied/TrialApplied.js
+++ client/src/pages/Protected/applied/TrialApplied.js
@@ -2,40 +2,59 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Container, Button, Form, Row, Col, Card } from "react-bootstrap";
 import { useParams, Link } from "react-router-dom";
-import trials from "../../../stimulate-backend/data/latest-trials.json";
+import { useNavigate } from "react-router-dom";
 
 export default function TrialApplied() {
   const { applied } = useSelector((state) => ({ ...state }));
   const { appliedID, url, trialTitle, participants, description } = applied;
-
+  const navigate = useNavigate();
   return (
     <Container className='card-container'>
       <h1 className='primary-color text-center'>Applied Trials</h1>
-      <Card className='card'>
-        <Card.Header className='card-header-css primary-font font-weight-bold'>
-          {trialTitle}
-        </Card.Header>
-        <Col>
-          <Card.Body>
-            <span className='font-weight-bold'>Particpants: </span>
-            {participants}
-          </Card.Body>
-          <Card.Text className='top5'>
-            <span className='font-weight-bold'>Description: </span>
-            {description}
-          </Card.Text>
-        </Col>
-        <Col>
-          <div>
-            <Link
-              to={`${url}/${appliedID}`}
-              className='primary-color d-flex justify-content-center link-btn'
+      {appliedID ? (
+        <Card className='card'>
+          <Card.Header className='card-header-css primary-font font-weight-bold'>
+            {trialTitle}
+          </Card.Header>
+          <Col>
+            <Card.Body>
+              <span className='font-weight-bold'>Particpants: </span>
+              {participants}
+            </Card.Body>
+            <Card.Text className='top5'>
+              <span className='font-weight-bold'>Description: </span>
+              {description}
+            </Card.Text>
+          </Col>
+          <Col>
+            <div>
+              <Link
+                to={`${url}/${appliedID}`}
+                className='primary-color d-flex justify-content-center link-btn'
+              >
+                Read More
+              </Link>
+            </div>
+          </Col>
+        </Card>
+      ) : (
+        <Card style={{ width: "20rem" }} className='height120 margin-left-50'>
+          <Card.Body className='top-80'>
+            <Card.Title className='text-center primary-color'>
+              You have not applied to any trials
+            </Card.Title>
+            <Card.Text className='text-center margin-top-sm'>
+              Please apply to your first trial.
+            </Card.Text>
+            <Button
+              onClick={() => navigate("/participate")}
+              className='primary-color-btn width-265 d-flex margin-auto justify-content-center'
             >
-              Read More
-            </Link>
-          </div>
-        </Col>
-      </Card>
+              Apply Now
+            </Button>
+          </Card.Body>
+        </Card>
+      )}
     </Container>
   );
 }
