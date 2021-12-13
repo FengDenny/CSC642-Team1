bb244aa1f74070f21ddc8c10fcb0bf4f16b271fa
FengDenny <fengdennyy@gmail.com>
2021-12-12 16:21:58 -0800

    Update TrialSubmitted.js

--- client/src/pages/Protected/clinical/TrialSubmitted.js
+++ client/src/pages/Protected/clinical/TrialSubmitted.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import CardSubmit from "../../Card/SubmitCard";
+import { Card, Container, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function TrialSubmitted() {
   const { auth } = useSelector((state) => ({ ...state }));
   const { submit } = useSelector((state) => ({ ...state }));
   const { clinicalID, companyName } = auth;
+  const navigate = useNavigate();
   const {
     title,
     trialID,
@@ -28,7 +31,24 @@ export default function TrialSubmitted() {
           btnName={"Learn More"}
         ></CardSubmit>
       ) : (
-        <p>Please create your first post!</p>
+        <Container className='card-container '>
+          <Card style={{ width: "18rem" }} className='height120'>
+            <Card.Body className='top-80'>
+              <Card.Title className='text-center primary-color'>
+                No Trials
+              </Card.Title>
+              <Card.Text className='text-center'>
+                Please create your first trial.
+              </Card.Text>
+              <Button
+                onClick={() => navigate("/submit-trials")}
+                className='primary-color-btn width-265 d-flex margin-auto justify-content-center'
+              >
+                Create Trial
+              </Button>
+            </Card.Body>
+          </Card>
+        </Container>
       )}
     </div>
   );
